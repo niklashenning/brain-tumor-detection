0249082cd971950c60caf58e29b18ba45854f1e9
Niklas Henning <niklashenningbusiness@gmail.com>
2024-10-21 20:40:21 +0200

    Optimize model

--- src/model.py
+++ src/model.py
@@ -6,11 +6,18 @@ class Model(nn.Module):
 
     def __init__(self):
         super(Model, self).__init__()
-        self.fc1 = nn.Linear(256 * 256, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64 * 64 * 64, 256)
+        self.fc2 = nn.Linear(256, 4)
+        self.dropout = nn.Dropout(0.25)
 
     def forward(self, x):
-        x = x.view(-1, 256 * 256)
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 64 * 64)
+        x = self.dropout(x)
         x = torch.relu(self.fc1(x))
         x = self.fc2(x)
         return x
